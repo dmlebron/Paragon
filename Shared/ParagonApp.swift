6f3df3325203dc3fa60bd0c5d48d94e5eed07e52
David Martinez-Lebron <davamale@gmail.com>
2021-10-19 13:44:23 -0400

    Initial Commit

---
+++ Shared/ParagonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParagonApp.swift
+//  Shared
+//
+//  Created by David Martinez on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct ParagonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
