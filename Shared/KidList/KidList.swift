9f2db2f89fa097857695fd042f3b7b87018cdba8
David Martinez-Lebron <davamale@gmail.com>
2021-10-20 23:27:22 -0400

    gitignore

---
+++ Shared/KidList/KidList.swift
@@ -0,0 +1,24 @@
+//
+//  KidList.swift
+//  Paragon
+//
+//  Created by David Martinez on 10/19/21.
+//
+
+import SwiftUI
+
+struct Kid {
+    let name: String
+}
+
+struct KidList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KidList_Previews: PreviewProvider {
+    static var previews: some View {
+        KidListView()
+    }
+}
---
+++ Shared/KidProfile/KidProfile.swift
@@ -0,0 +1,20 @@
+//
+//  KidProfile.swift
+//  Paragon
+//
+//  Created by David Martinez on 10/19/21.
+//
+
+import SwiftUI
+
+struct KidProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KidProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        KidProfile()
+    }
+}
